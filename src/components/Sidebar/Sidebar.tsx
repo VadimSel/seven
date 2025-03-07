357c19b12ea37a20583b1fef8b13f877f9e5f2f5
VadimSel <selyaaa33@gmail.com>
2025-03-07 21:17:44 +0300

    add sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,38 @@
-import styles from './Sidebar.module.scss'
+import styles from "./Sidebar.module.scss";
+
+const data: string[] = [
+	"По проекту",
+	"Объекты",
+	"РД",
+	"МТО",
+	"СМР",
+	"График",
+	"МиМ",
+	"Рабочие",
+	"Капвложения",
+	"Бюджет",
+	"Финансирование",
+	"Панорамы",
+	"Камеры",
+	"Поручения",
+	"Контрагенты",
+];
 
 const Sidebar = () => {
-	return <div className={styles.container}>Sidebar</div>;
+	return (
+		<div className={styles.container}>
+			<ul>
+				{data.map((el, i) => {
+					return (
+						<div className={styles.element_wrapper}>
+							<img src="/assets/icons/project-icon.svg" alt="" />
+							<li className={styles.element}>{el}</li>
+						</div>
+					)
+				})}
+			</ul>
+		</div>
+	);
 };
 
 export default Sidebar;
